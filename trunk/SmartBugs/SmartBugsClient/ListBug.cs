1774cb9881c37503e2463ab3ac0c84b8ab925822
andrei <andrei@c66a092f-ee6a-ef42-9785-75605c6bdf6b>
2004-05-26 20:42:22 +0000

    Added xml parsing and label generation.

--- trunk/SmartBugs/SmartBugsClient/ListBug.cs
+++ trunk/SmartBugs/SmartBugsClient/ListBug.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Net;
+using System.IO;
 using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SmartBugsClient
 {
@@ -11,8 +14,16 @@ namespace SmartBugsClient
 	/// </summary>
 	public class ListBug : System.Windows.Forms.Form
 	{
+		private string configString;
+		private XmlDocument config;
+		private ArrayList labels = new ArrayList();
+		private ArrayList constrols = new ArrayList();
+
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button getConfig;
+		private System.Windows.Forms.Panel bugControls;
+
 
 		/// <summary>
 		/// Required designer variable.
@@ -53,15 +64,15 @@ namespace SmartBugsClient
 		/// </summary>
 		private void InitializeComponent()
 		{
-			// for each label in the labelArray we assign a new System.Windows.Forms
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
-
+			this.getConfig = new System.Windows.Forms.Button();
+			this.bugControls = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(8, 8);
+			this.label1.Location = new System.Drawing.Point(8, 48);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(80, 16);
 			this.label1.TabIndex = 0;
@@ -69,16 +80,38 @@ namespace SmartBugsClient
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(112, 8);
+			this.label2.Location = new System.Drawing.Point(112, 48);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(144, 16);
 			this.label2.TabIndex = 1;
 			this.label2.Text = "title";
 			// 
+			// getConfig
+			// 
+			this.getConfig.Location = new System.Drawing.Point(8, 8);
+			this.getConfig.Name = "getConfig";
+			this.getConfig.TabIndex = 2;
+			this.getConfig.Text = "Get Config";
+			this.getConfig.Click += new System.EventHandler(this.getConfig_Click);
+			// 
+			// bugControls
+			// 
+			this.bugControls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+				| System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right)));
+			this.bugControls.AutoScroll = true;
+			this.bugControls.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.bugControls.Location = new System.Drawing.Point(8, 72);
+			this.bugControls.Name = "bugControls";
+			this.bugControls.Size = new System.Drawing.Size(520, 296);
+			this.bugControls.TabIndex = 3;
+			// 
 			// ListBug
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(292, 266);
+			this.ClientSize = new System.Drawing.Size(536, 373);
+			this.Controls.Add(this.bugControls);
+			this.Controls.Add(this.getConfig);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Name = "ListBug";
@@ -87,5 +120,64 @@ namespace SmartBugsClient
 
 		}
 		#endregion
+
+		public static void Main()
+		{
+			Application.Run(new ListBug());
+		}
+
+		private void getConfig_Click(object sender, System.EventArgs e)
+		{
+			HttpWebRequest request = (HttpWebRequest) 
+				WebRequest.Create("http://localhost/SmartBugsServer/Bug.xml");
+			request.Method = "GET";
+			WebResponse response = request.GetResponse();
+			StreamReader sr = new StreamReader(response.GetResponseStream());
+			configString = sr.ReadToEnd();
+			config = new XmlDocument();
+			config.LoadXml(configString);
+			addEditControls(config.FirstChild.NextSibling);
+			sr.Close();
+		}
+
+		private void addEditControls(XmlNode node)
+		{
+			int y = 10;
+			for (XmlNode field = node.FirstChild; field != null; field = field.NextSibling) 
+			{
+				if (field.Name == "field")
+				{
+					Label label = new Label();
+					label.Location = new System.Drawing.Point(10, y);
+					label.Size = new System.Drawing.Size(380, 16);
+					label.Text = field.Attributes["title"].Value;
+					bugControls.Controls.Add(label);
+					//this.label1.Name = "label1";
+					//this.label1.TabIndex = 0;
+					y += 22;
+				}
+				else 
+				{
+					Label label = new Label();
+					label.Location = new System.Drawing.Point(10, y);
+					label.Size = new System.Drawing.Size(380, 16);
+					label.Text = field.Attributes["title"].Value;
+					bugControls.Controls.Add(label);
+					//this.label1.Name = "label1";
+					//this.label1.TabIndex = 0;
+					y += 22;
+
+					Panel group = new Panel();
+					group.AutoScroll = true;
+					group.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+					group.Location = new System.Drawing.Point(5, y);
+					//this.bugControls.Name = "bugControls";
+					group.Size = new System.Drawing.Size(510, 296);
+					bugControls.Controls.Add(group);
+					y += 296 + 6;
+				}
+			}
+			this.ResumeLayout(false);
+		}
 	}
 }
